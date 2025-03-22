8ba6f2213add2c8251dc1cb97ab841972f8ea695
tomlord <aa123593465@gmail.com>
2025-03-22 18:33:58 +0800

    feat: new script to load new font style

--- mv_font.sh
+++ mv_font.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# 強制自動執行
-set -e  # 發生錯誤時立即停止執行
+set -e
 
 FONT_DIR="$HOME/Downloads/Kaiu.ttf"
 PROJECT_DIR="$HOME/helphelp-titi/static/fonts"
 
-# 檢查文件是否存在
 if [ ! -f "$FONT_DIR" ]; then
   echo "Error: Font file not found at $FONT_DIR"
   exit 1
 fi
 
-# 移動文件到專案目錄
 mv "$FONT_DIR" "$PROJECT_DIR"
 
 echo "Font file moved to $PROJECT_DIR"
 
-# 列出專案目錄中的文件
